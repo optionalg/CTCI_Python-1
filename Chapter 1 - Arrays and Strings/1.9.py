33d3d71c5344159f06f80e9457d414e95d136592
Darshan Patel <darshan97@gmail.com>
2017-09-30 11:23:46 -0400

    check if one string is rotation of other using isSubstring()

--- Chapter 1 - Arrays and Strings/1.9.py
+++ Chapter 1 - Arrays and Strings/1.9.py
@@ -4,3 +4,16 @@ Assume you have a method isSubstring which checks if one word is a substring of 
  Given two strings, sl and s2, write code to check if s2 is a rotation of sl 
  using only one call to isSubstring (e.g.,"waterbottle" is a rotation of"erbottlewat").
 """
+
+def isRotatedString(s1, s2):
+    if not s1 or not s2:
+        return False
+    if len(s1) != len(s2):
+        return False
+
+    return isSubstring(s1 + s1, s2)
+
+def isSubstring(s1, s2):
+    return s2 in s1
+
+print(isRotatedString("waterbottle", "erbottlewat"))
