4af9e0bf688f5044cd5ce6fd5207d2148af04e92
Darshan Patel <darshan97@gmail.com>
2017-09-29 21:21:02 -0400

    compress string with repeated characters frequencies

--- Chapter 1 - Arrays and Strings/1.6.py
+++ Chapter 1 - Arrays and Strings/1.6.py
@@ -2,7 +2,7 @@
 String Compression: Implement a method to perform basic string compression using the counts of repeated characters. 
 For example, the string aabcccccaaa would become a2blc5a3. 
 If the "compressed" string would not become smaller than the original string, your method should return
-the original string. You can assume the string has only uppercase and lowercase letters (a - z).
+the original string. You can assume the string has only uppercase and lowercase letters (a - z). 
 """
 def stringCompression(word):
     if len(word) <= 2:
